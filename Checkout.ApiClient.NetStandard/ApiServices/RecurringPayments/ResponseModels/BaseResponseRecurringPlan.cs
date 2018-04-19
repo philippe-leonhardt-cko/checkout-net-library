4262312f362573f66b271a3c4d6bfd3557f1f97c
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2018-04-19 10:40:16 +0200

    Updated comment

--- Checkout.ApiClient.NetStandard/ApiServices/RecurringPayments/ResponseModels/BaseResponseRecurringPlan.cs
+++ Checkout.ApiClient.NetStandard/ApiServices/RecurringPayments/ResponseModels/BaseResponseRecurringPlan.cs
@@ -12,19 +12,19 @@ namespace Checkout.ApiServices.RecurringPayments.ResponseModels
         public string Name { get; set; }
         public string PlanTrackId { get; set; }
         public decimal? AutoCapTime { get; set; }
-
+        // Value must be an int but some API endpoints return a decimal
+        // This handles the assignment of the Value property and converts from decimal to int, if necessary
+        #region Value Type Handling
         [JsonIgnore]
         public int Value
         {
             get { return (int) _privateValue; }
             set { _privateValue = value; }
         }
-
-        // TODO: Explain reason
         [JsonProperty(PropertyName = "value")]
         [Obsolete]
         public decimal _privateValue { get; set; }
-
+        #endregion
         public string Cycle { get; set; }
         public int? RecurringCount { get; set; }
     }
